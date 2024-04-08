afd6e98890f7d0cc178e67e03b0a752901fcfe8b
norisuke <norimasajave@gmail.com>
2024-04-08 20:48:11 +0900

    七並べテーブル

--- src/fantan/FantanTable.java
+++ src/fantan/FantanTable.java
@@ -16,6 +16,46 @@ public class FantanTable implements Table {
      * @param card
      */
     public void putCard(Card[] card) {
+        int number = card[0].getNumber();
+        int suit = card[0].getSuit();
 
+        // テーブルにカードを置く
+        table_[suit - 1][number - 1] = card[0];
+    }
+
+    /**
+     * カードを見る
+     * 
+     * @return テーブルに置かれたカードを表示する
+     */
+    public Card[][] getCards() {
+        Card[][] table = new Card[Card.SUIT_NUM][Card.CARD_NUM];
+        for (int suit = 0; suit < Card.SUIT_NUM; suit++) {
+            System.arraycopy(table_[suit], 0, table[suit], 0, Card.CARD_NUM);
+        }
+
+        return table;
+    }
+
+    /**
+     * テーブルを文字列で表す
+     */
+    public String toString() {
+        StringBuffer tableString = new StringBuffer(200);
+        for (int suit = 0; suit < Card.SUIT_NUM; suit++) {
+            for (int number = 0; number < Card.CARD_NUM; number++) {
+                if (table_[suit][number] != null) {
+                    tableString.append(table_[suit][number]);
+                } else {
+                    tableString.append("..");
+                }
+
+                tableString.append(" ");
+            }
+
+            tableString.append("￥n");
+        }
+
+        return tableString.toString();
     }
 }
